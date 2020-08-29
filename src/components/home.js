5ffc760dd37a8af62cdcb0f6e7be6c5d5d7ec58d
Abby Krishnan <abbykrishnan16@gmail.com>
2020-08-28 20:33:16 -0500

    made everything work

--- src/components/home.js
+++ src/components/home.js
@@ -6,11 +6,14 @@ import { SSL_OP_NETSCAPE_REUSE_CIPHER_CHANGE_BUG } from "constants"
 
 function searchInContributions(toFind){
   toFind = toFind.toLowerCase();
+  console.log(Object.keys(Contributions[0].contributionSummary).map(pac => pac.toLowerCase()))
+  console.log(Object.keys(Contributions[0].contributionSummary).map(pac => pac.toLowerCase()).findIndex(i => i.includes(toFind)))
+
   const toRet = Contributions.filter(
     (contribution) =>
-        contribution.policeDeptPac.toLowerCase().includes(toFind) || 
-        contribution.electedOfficialName.toLowerCase().includes(toFind) || 
-        contribution.officeRanForOrDescription.toLowerCase().includes(toFind)
+        contribution.electedOfficialName.toLowerCase().includes(toFind) ||
+        contribution.fullPosition.toLowerCase().includes(toFind) ||
+        Object.keys(contribution.contributionSummary).map(pac => pac.toLowerCase()).findIndex(i => i.includes(toFind)) >= 0
     )
   console.log(toRet)
   return toRet;
@@ -30,12 +33,12 @@ export default function Home() {
           In 2019 alone, nearly half a million dollars were donated to campaigns for elected office across the Senate. Most of these are local, city-wide positions. How can we expect our local Representatives, DAs and more to commit to police reform when they rely on their contribution?
           </div>
         </div>
-        <div> 
-          <input 
+        <div>
+          <input
             className={styles.search}
-            value={searchTerm} 
+            value={searchTerm}
             placeholder="Search"
-            onChange={(event) => {setSearchTerm(event.target.value); setCardsToDisplay(searchInContributions(event.target.value))}} 
+            onChange={(event) => {setSearchTerm(event.target.value); setCardsToDisplay(searchInContributions(event.target.value))}}
           />
           <Cards cards={cardsToDisplay}/>
         </div>
